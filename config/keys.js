53c2c7c0ff528791509a3350210292143af12ed8
samrat <samratshakya5@gmail.com>
2020-06-14 14:12:20 +0545

    initial express server routes

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://devconnector:connect123@samrat-lcvru.mongodb.net/samrat?retryWrites=true&w=majority",
+};
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
+
+//DB CONFIGS
+
+const db = require("./config/keys").mongoURI;
+
+//Connect to mongodb
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello Worldsd"));
+
+//USE ROUTES
+
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
