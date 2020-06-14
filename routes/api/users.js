dd8ce1b6686062a38f917f7954b13f951ef2b6de
samrat <samratshakya5@gmail.com>
2020-06-14 22:49:30 +0545

    passport implementation

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport");
 
 //Load User model
 const User = require("../../models/User");
@@ -68,17 +69,38 @@ router.post("/login", (req, res) => {
       if (isMatch) {
         //user matched
         const payload = { id: user.id, name: user.name, avatar: user.avatar };
-        jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
-          res.json({
-            success: "true",
-            token: "Bearer " + token,
-          });
-        });
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: "true",
+              token: "Bearer " + token,
+            });
+          }
+        );
       } else {
         return res.status(400).json({ password: "Password Incorrect" });
       }
     });
   });
 });
+
+//@route GET api/users/current
+//@desc Return Current User
+//@access Private
+
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email,
+    });
+  }
+);
 
 module.exports = router;
