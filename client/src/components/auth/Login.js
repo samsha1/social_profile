5bb684c91f3f9c114b73730a947810df5874c42d
samrat <samratshakya5@gmail.com>
2020-06-24 00:40:49 +0545

    redux setup and register user and adding error to redux store

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,75 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {},
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const loginData = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    console.log(loginData);
+
+    axios
+      .post("/api/users/login", loginData)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err.response.data));
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <div class="login">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8 m-auto">
+              <h1 class="display-4 text-center">Log In</h1>
+              <p class="lead text-center">
+                Sign in to your DevConnector account
+              </p>
+              <form noValidate onSubmit={this.onSubmit}>
+                <div class="form-group">
+                  <input
+                    type="email"
+                    class="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    onChange={this.onChange}
+                    value={this.state.email}
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    type="password"
+                    class="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    onChange={this.onChange}
+                    value={this.state.password}
+                  />
+                </div>
+                <input type="submit" class="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
